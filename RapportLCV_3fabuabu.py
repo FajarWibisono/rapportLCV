67aa5c7a56ef6746a4233278ff4dae6afe1c8dfd
FajarWibisono <73801942+FajarWibisono@users.noreply.github.com>
2025-12-03 17:05:13 +0700

    Update RapportLCV_3fabuabu.py
    ganti ke gpt5-nano

--- RapportLCV_3fabuabu.py
+++ RapportLCV_3fabuabu.py
@@ -16,7 +16,7 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tessera
 
 # Konfigurasi API OpenAI (AMAN melalui secrets)
 OPENAI_API_KEY = st.secrets["openai"]["api_key"]
-OPENAI_MODEL = "gpt-4o"  # atau "gpt-4o-mini" jika ingin lebih hemat
+OPENAI_MODEL = "gpt-5-nano"  # atau "gpt-4o-mini" jika ingin lebih hemat
 
 # Konfigurasi halaman
 st.set_page_config(
@@ -840,4 +840,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
